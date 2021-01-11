af3a85f62123483ddc4d380a058e22dc4103767c
Marcello Del Buono <m.delbuono@gmail.com>
2021-01-11 15:17:33 +0100

    Fixed path resolution bug

--- test/package/.olojs/config.js
+++
@@ -1,9 +0,0 @@
-const olojs = require('@onlabsorg/olojs');
-
-exports.routes = {
-    '/': new olojs.FileStore(`${__dirname}/..`),
-};
-
-exports.servers = {
-    default: store => olojs.HTTPServer.createServer(store)
-}
