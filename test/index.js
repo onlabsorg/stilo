80eb0f06661870594fde20b9f987a7ea2f2d5258
Marcello Del Buono <m.delbuono@gmail.com>
2022-06-19 21:37:36 +0200

    Added --output and --verbose option to stilo-render command

--- test/index.js
+++ test/index.js
@@ -179,6 +179,20 @@ describe("stilo CLI", () => {
             process.chdir(cwd);            
             process.stdout.write = stdout_write;
         });
+
+        it("should output the rendered document to the file path eventually passed with the --output option", async () => {
+            // Status snapshot
+            const cwd = process.cwd();
+            
+            // Run the commands
+            process.chdir( pathlib.join(__dirname, 'test-repository') );
+            await stilo.render('/dir/doc1', {output:"dir/doc1.txt"});
+            const outputFileContent = fs.readFileSync(pathlib.join(__dirname, 'test-repository/dir/doc1.txt'), 'utf8')
+            expect(outputFileContent).to.equal("document @ /dir/doc1\n");
+
+            // Restore original status
+            process.chdir(cwd);            
+        });
     });
 
     describe("stilo.serve(path, {port})", () => {
