87fbe428cc6f9d60ac4013dc25c4543cbf4fcd59
Marcello Del Buono <m.delbuono@gmail.com>
2022-06-19 21:28:05 +0200

    Added --output and --verbose options to stilo-read command

--- test/index.js
+++ test/index.js
@@ -119,9 +119,24 @@ describe("stilo CLI", () => {
             process.chdir(cwd);            
             process.stdout.write = stdout_write;
         });
+
+        it("should output the source to the file path eventually passed with the --output option", async () => {
+            // Status snapshot
+            const cwd = process.cwd();
+            
+            // Run the commands
+            process.chdir( pathlib.join(__dirname, 'test-repository') );
+            await stilo.read('/dir/doc1', {output:"dir/doc1.txt"});
+            const outputFileContent = fs.readFileSync(pathlib.join(__dirname, 'test-repository/dir/doc1.txt'), 'utf8')
+            const sourceFileContent = fs.readFileSync(pathlib.join(__dirname, 'test-repository/dir/doc1.olo'), 'utf8')
+            expect(outputFileContent).to.equal(sourceFileContent);
+
+            // Restore original status
+            process.chdir(cwd);            
+        });
     });
     
-    describe("stilo.render(docPAth)", () => {
+    describe("stilo.render(docPath)", () => {
 
         it("should fetch the doc mapped to docPath and print its rendered text to stdout ", async () => {
             let stdout = "";
